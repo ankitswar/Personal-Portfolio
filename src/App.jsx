5fcaa44c289920a88c60a72f3afdd2d65610f13f
ankitswar <118311603+ankitswar@users.noreply.github.com>
2025-01-13 00:24:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import Education from "./components/Education";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
     <Skills/>
     <Portfolio/>
     <Experience/>
+    <Education/>
     <Contact/>
     </>
   )
